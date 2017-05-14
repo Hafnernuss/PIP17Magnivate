533432ee8d5e9c1fe0465366b2f9a524a0453c8a
Philipp Hafner <Philipp Hafner>
2017-05-14 17:34:29 +0200

    first working version of the pwm controlled motor setup

--- MotorControl/MotorControl.ino
+++ MotorControl/MotorControl.ino
@@ -25,95 +25,65 @@
 
 #define MOTOR_BASE_PIN 3
 
-#define MOTOR_SPEED_MIN 15
+#define MOTOR_SPEED_MIN 14
 #define MOTOR_SPEED_MAX 255
 #define MOTORSPEEDSTR "Motor Speed: %d%%"
 #define PERCENT_TO_SPEED(x) (((x * (MOTOR_SPEED_MAX - MOTOR_SPEED_MIN)) / 100)) + MOTOR_SPEED_MIN
 #define ADC_TO_PERCENT(x) (long)((((long)x) * (long)100) / (long)1020)
 
-
-#define BTN_SPD_UP 5
-#define BTN_SPD_DOWN 4
-
 #define POT_PIN 0
 
-void OnSpeedUpPressed();
-void OnSpeedUpReleased(unsigned long nPressTime);
-void OnSpeedDownPressed();
-void OnSpeedDownReleased(unsigned long nPressTime);
 void ReadPotentiometer(Task * This);
 
-
 LiquidCrystal lcd(LCD_RS, LCD_EN, LCD_D4, LCD_D5, LCD_D6, LCD_D7);
-Debouncer BtnSpeedUp(BTN_SPD_UP, MODE_CLOSE_ON_PUSH, NULL, OnSpeedUpReleased);
-Debouncer BtnSpeedDown(BTN_SPD_DOWN, MODE_CLOSE_ON_PUSH, NULL, OnSpeedDownReleased);
-
-int nPercentage = 0;
-char cScreenBuf[255];
-
 Task ReadPotentiometerTask(100, ReadPotentiometer);
 
+bool bFirstRun = true;
 
 void setup() 
 {
   lcd.begin(LCD_NUM_COLUMNS, LCD_NUM_ROWS);
 
   pinMode(MOTOR_BASE_PIN, OUTPUT);
-  PciManager.registerListener(BTN_SPD_UP, &BtnSpeedUp);
-  PciManager.registerListener(BTN_SPD_DOWN, &BtnSpeedDown);
-  pinMode(MOTOR_BASE_PIN, OUTPUT);
 
   SetMotorSpeed(0);
-  SetPWMToMotor();
 
   SoftTimer.add(&ReadPotentiometerTask);
 
 }
 
-void OnSpeedUpReleased(unsigned long nPressTime)
-{
- while(digitalRead(BTN_SPD_UP) == HIGH)
- {
-   SetMotorSpeed(nPercentage + 1);
-   SetPWMToMotor();
-   delay(100);
-  }
-}
-
-void OnSpeedDownReleased(unsigned long nPressTime)
-{
-  while(digitalRead(BTN_SPD_DOWN) == HIGH)
- {
-   SetMotorSpeed(nPercentage - 1);
-   SetPWMToMotor();
-   delay(100);
-  }
-}
-
-void SetPWMToMotor()
-{
-  int nMotorSpeed = PERCENT_TO_SPEED(nPercentage);
-  if(nPercentage == 0)
-    nMotorSpeed = 0;
-  analogWrite(MOTOR_BASE_PIN, nMotorSpeed);
-}
-
 void SetMotorSpeed(int nNewPercentage)
 {
-  nPercentage = constrain(nNewPercentage, 0, 100);
+  int nPercentage = constrain(nNewPercentage, 0, 100);
+  char cScreenBuf[255];
+  
   lcd.clear();
   sprintf(cScreenBuf, MOTORSPEEDSTR, nPercentage);
   lcd.print(cScreenBuf);
+
+  int nMotorSpeed = PERCENT_TO_SPEED(nPercentage);
+  if(nPercentage == 0)
+    nMotorSpeed = 0;
+    
+  analogWrite(MOTOR_BASE_PIN, nMotorSpeed);
 }
 
 void ReadPotentiometer(Task * This)
 {
   int val = analogRead(POT_PIN); 
-  lcd.setCursor(0,1);
-  lcd.write("      ");
-  lcd.setCursor(0,1);
-  lcd.print(ADC_TO_PERCENT(val));
-  SetMotorSpeed(ADC_TO_PERCENT(val));
-  SetPWMToMotor();
+  int nPercentage = ADC_TO_PERCENT(val);
+  if(bFirstRun && nPercentage != 0)
+  {
+    lcd.setCursor(0,1);
+    lcd.print("FUSED.");
+  }
+    
+  else
+  {
+    bFirstRun = false; 
+    SetMotorSpeed(nPercentage);
+  }
+    
+  
 }
 
