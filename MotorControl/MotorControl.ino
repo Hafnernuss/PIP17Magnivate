908c839bb12d8593cea0ad240cf4821607bf34aa
Philipp Hafner <Philipp Hafner>
2017-05-28 16:01:18 +0200

    the final code.

--- MotorControl/MotorControl.ino
+++ MotorControl/MotorControl.ino
@@ -13,64 +13,56 @@
 
 #include <LiquidCrystal.h>
 
-#define LCD_RS 12
-#define LCD_EN 11
-#define LCD_D4 10
-#define LCD_D5 9
-#define LCD_D6 8
-#define LCD_D7 7
+#define LCD_RS 11
+#define LCD_EN 9
+#define LCD_D4 7
+#define LCD_D5 6
+#define LCD_D6 5
+#define LCD_D7 4
 
 #define LCD_NUM_ROWS 4
-#define LCD_NUM_COLUMNS 20
+#define LCD_NUM_COLUMNS 16
 
-#define MOTOR_BASE_PIN 5
+#define MOTOR_BASE_PIN 3
 
 #define MOTOR_SPEED_MIN 14
 #define MOTOR_SPEED_MAX 255
-#define MOTORSPEEDSTR "Mot Speed: %d%%"
+#define MOTORSPEEDSTR "MotorSpeed: %d%%"
+#define FUSEDSTR "FUSED."
 #define PERCENT_TO_SPEED(x) (((x * (MOTOR_SPEED_MAX - MOTOR_SPEED_MIN)) / 100)) + MOTOR_SPEED_MIN
 #define ADC_TO_PERCENT(x) (long)((((long)x) * (long)100) / (long)1020)
 
-#define POT_PIN 0
-#define IRTRANSISTOR_PIN 5
-
-#define IR_THRESHOLD 20
-#define IS_IR_ON(x) (x >= 20)
-#define IS_IR_OFF(x) (!IS_IR_ON(x))
+#define POT_PIN 4
 
 void ReadPotentiometer(Task * This);
-void ReadIRTransistor(Task * This);
-void CalculateRPM(Task * This);
 
 LiquidCrystal lcd(LCD_RS, LCD_EN, LCD_D4, LCD_D5, LCD_D6, LCD_D7);
 Task ReadPotentiometerTask(100, ReadPotentiometer);
-Task ReadIRTransistorTask(10, ReadIRTransistor);
-Task CalculateRPMTask(1000, CalculateRPM);
 
 bool bFirstRun = true;
-bool bLastIRState = false;
-int nTicksPer500MS = 0;
+bool bFused = false;
 
 void setup() 
 {
   lcd.begin(LCD_NUM_COLUMNS, LCD_NUM_ROWS);
+  lcd.setCursor(0,0);
+  lcd.print("-- MAGNIVATE --");
 
   pinMode(MOTOR_BASE_PIN, OUTPUT);
 
   SetMotorSpeed(0);
 
   SoftTimer.add(&ReadPotentiometerTask);
-  SoftTimer.add(&ReadIRTransistorTask);
-  SoftTimer.add(&CalculateRPMTask);
-
 }
 
 void SetMotorSpeed(int nNewPercentage)
 {
   int nPercentage = constrain(nNewPercentage, 0, 100);
   char cScreenBuf[255];
-
-  lcd.setCursor(0,0);
+  
+  lcd.setCursor(0,1);
+  lcd.print("                ");
+  lcd.setCursor(0,1);
   sprintf(cScreenBuf, MOTORSPEEDSTR, nPercentage);
   lcd.print(cScreenBuf);
 
@@ -83,51 +75,20 @@ void SetMotorSpeed(int nNewPercentage)
 
 void ReadPotentiometer(Task * This)
 {
-  lcd.setCursor(0,1);
+  lcd.setCursor(0,2);
   int val = analogRead(POT_PIN); 
   int nPercentage = ADC_TO_PERCENT(val);
   if(bFirstRun && nPercentage != 0)
   {
     lcd.setCursor(0,1);
-    lcd.print("FUSED.");
+    lcd.print("                ");
+    lcd.setCursor(0,1);
+    lcd.print(" --- FUSED --- ");
   }
     
   else
   {
-    lcd.print("      ");
     bFirstRun = false; 
     SetMotorSpeed(nPercentage);
   }
 }
-
-void ReadIRTransistor(Task * This)
-{
-  lcd.setCursor(0,3);
-  
-  
-  int val = analogRead(IRTRANSISTOR_PIN); 
-  bool bState = IS_IR_ON(val);
-  
-  if(bState != bLastIRState)
-  {
-    bLastIRState = bState;
-    nTicksPer500MS++;
-  }
-
-  
-}
-
-
-void CalculateRPM(Task * This)
-{
-  lcd.setCursor(0,3);
-  lcd.print("      ");
-  lcd.setCursor(0,3);
-  lcd.print(nTicksPer500MS);
- // lcd.print(nTicksPer500MS);
- // int RPM = nTicksPer500MS / 500;
-  nTicksPer500MS = 0;
-  
-  
-}
-
